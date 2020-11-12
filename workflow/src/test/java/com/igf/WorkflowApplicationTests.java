b3eccdbb2fced8b5e38debeb31dbf6d95d302224
Alejandro Martínez <alejandro2ml22@gmail.com>
2020-11-12 16:43:04 -0600

    Configuracion a base de datos

---
+++ workflow/src/test/java/com/igf/WorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
