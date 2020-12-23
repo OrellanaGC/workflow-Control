d12d0644f65868f10baf51d12497a6b359eba0c3
Brandon <daniel16parrillas@gmail.com>
2020-12-22 18:32:07 -0600

    Permission_Roles

---
+++ workflow/src/main/java/com/igf/modelo/Permission_Role.java
@@ -0,0 +1,35 @@
+package com.igf.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity 
+@Table(name = "permission_role")
+public class Permission_Role {
+	@Id
+	@GeneratedValue
+	private Long id;
+	@ManyToOne	
+	@JoinColumn(name ="id_permission", nullable = false)	
+	private Permission permission;
+	public Permission_Role() {
+		
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Permission getPermission() {
+		return permission;
+	}
+	public void setPermission(Permission permission) {
+		this.permission = permission;
+	}
+	
+}
