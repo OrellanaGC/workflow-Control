62db078acd65a042b75bc7d30061051a91c46636
Brandon <daniel16parrillas@gmail.com>
2020-12-22 19:36:02 -0600

    TablasyRepositorios

---
+++ workflow/src/main/java/com/igf/negocio/repositorio/Permission_RoleRepository.java
@@ -0,0 +1,9 @@
+package com.igf.negocio.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.igf.modelo.Permission_Role;
+
+public interface Permission_RoleRepository extends JpaRepository<Permission_Role, Long>{
+
+}
