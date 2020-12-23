7ca95646b2d7ed1e8a914bb35f658c3535a7e660
vanessa <mc15114@ues.edu.sv>
2020-12-22 18:17:06 -0600

    permission role arreglo

--- workflow/src/main/java/com/igf/modelo/Permission_role.java
+++ workflow/src/main/java/com/igf/modelo/Permission_role.java
@@ -1,57 +1,30 @@
-/**
- * 
- */
 package com.igf.modelo;
-
-import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-/**
- * @author Pc
- *
- */
 @Entity
-@Table(name = "permission_role")
-public class Permission_role {
-	@Id
-	@GeneratedValue
-	private Long id;
-	
-	@OneToMany(mappedBy ="permission_role")
-	private Set<Permission_role>permission;
-	
+@Table(name= "permission_role")
+public class permission_role {
+@Id
+@GeneratedValue
+	private int id;
 
-	public Permission_role(Long id) {
+	public permission_role() {
 		
-		this.id = id;
 	}
 
-	
-
-	public Set<Permission_role> getPermission() {
-		return permission;
-	}
-
-	public void setPermission(Set<Permission_role> permission) {
-		this.permission = permission;
-	}
-
-
-
-	public Long getId() {
+	public int getId() {
 		return id;
 	}
 
-
-
-	public void setId(Long id) {
+	public void setId(int id) {
 		this.id = id;
 	}
+
+	
 	
 	
 }
