74b1bee5ae6bf4d0c93347fe328aaa253f198efd
David Deras <dc19019@ues.edu.sv>
2021-01-12 11:47:08 -0600

    seeder v1
    incorporacion seed para base de datos (user)

--- workflow/src/main/java/com/igf/WorkflowApplication.java
+++ workflow/src/main/java/com/igf/WorkflowApplication.java
@@ -1,23 +1,52 @@
 package com.igf;
 
+import java.sql.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.igf.controllers.FileUploadController;
+import com.igf.modelo.User;
+import com.igf.negocio.servicios.UserService;
+
 @Controller
 @SpringBootApplication
 public class WorkflowApplication {
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private FileUploadController fileUploadController;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(WorkflowApplication.class, args);
-		
 	}
 	
 	@GetMapping
 	public String index(Model model) {
 		return "redirect:/diagramas";
+	}
+	
+	@EventListener
+	public void seed(ContextRefreshedEvent event) {
+		if(!userService.exists("admin@admin")) {
+			User user = new User();
+			user.setName("admin");
+			user.setEmail("admin@admin.com");
+			user.setPassword("123456789");
+			user.setSexo("Hombre");
+			Date date = new Date(System.currentTimeMillis());
+			user.setFecha_nac(date);
+			userService.save(user);
+			fileUploadController.setUser(user);
+		} else {
+			fileUploadController.setUser(userService.find("admin@admin").get());
+		}
 	}
 
 }
--- workflow/src/main/java/com/igf/controllers/FileUploadController.java
+++ workflow/src/main/java/com/igf/controllers/FileUploadController.java
@@ -3,15 +3,12 @@ package com.igf.controllers;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.sql.Date;
-import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,18 +16,23 @@ import org.springframework.web.multipart.MultipartFile;
 import com.igf.modelo.Diagrama;
 import com.igf.modelo.User;
 import com.igf.negocio.servicios.DiagramaService;
-import com.igf.negocio.servicios.UserService;
-
-import antlr.collections.List;
 
 @Controller
 @RequestMapping("/fileupload")
 public class FileUploadController {
 	@Autowired	
 	private DiagramaService diagramaService;
-	@Autowired
-	private UserService userService;
+
+	private User usuario;
 	
+	public User getUser() {
+		return usuario;
+	}
+
+	public void setUser(User user) {
+		this.usuario = user;
+	}
+
 	public static String uploadDirectory= System.getProperty("user.dir")+"/diagramas";
 	
 	@GetMapping("")
@@ -45,7 +47,7 @@ public class FileUploadController {
 		StringBuilder filenames= new StringBuilder();
 		for(MultipartFile file : files) {
 			String nombre= file.getOriginalFilename();
-			//Validación de documento nulo
+			//Validacion de documento nulo
 			if(file.getSize()<=0) {
 				model.addAttribute("msg", "Tiene que subir un archivo");				
 				return "/upload/uploadPage";
@@ -59,12 +61,12 @@ public class FileUploadController {
 				model.addAttribute("msg", "Este diagrama ha sido subido al sistema con interioridad");				
 				return "/upload/uploadPage";
 			}
-			//Validación de archivos mayor a 5 MB
+			//Validacion de archivos mayor a 5 MB
 			if(file.getSize()>5000000) {
 				model.addAttribute("msg", "El archivo que intenta subir es superior a 5 MB");				
 				return "/upload/uploadPage";
 			}			
-			//Validación de archivos permitidos, xml y proc
+			//Validacion de archivos permitidos, xml y proc
 			if(!file.getOriginalFilename().endsWith("xml") && !file.getOriginalFilename().endsWith("proc")) {
 				model.addAttribute("msg", "solo se permiten archivos con extension '.xml' o '.proc'");				
 				return "/upload/uploadPage";
@@ -76,7 +78,6 @@ public class FileUploadController {
 				Diagrama diagrama = new Diagrama();
 				diagrama.setNombre(nombre);
 				diagrama.setPathArchivo(uploadDirectory+"/"+file.getOriginalFilename());
-				User usuario= userService.find("alejandro@mailinator.com").get();
 				/* Prueba de manejo de fechas con springboot
 				System.out.println(usuario.toString());				
 				System.out.println(usuario.getFecha_nac());
